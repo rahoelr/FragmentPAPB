8f25bcc4951b7709422dab5d99e0baf8e3837341
rahoelr <rahul.rahmatullah@mail.ugm.ac.id>
2023-02-28 03:04:09 +0700

    Dynamic Beta

--- app/src/main/java/com/example/fragmentnewww/MainActivity.java
+++ app/src/main/java/com/example/fragmentnewww/MainActivity.java
@@ -1,18 +1,57 @@
 package com.example.fragmentnewww;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_next;
     private boolean isFragmentDisplayed = false;
+    private View fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_next = findViewById(R.id.button_next);
+        fragment = findViewById(R.id.fragment1);
+
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isFragmentDisplayed){
+                    displayFragment();
+                } else {
+                    closeFragment();
+                }
+            }
+        });
+
+    }
+    public void displayFragment(){
+        SecondFragment secondFragment = SecondFragment.newInstance();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment1, secondFragment).addToBackStack(null).commit();
+        fragment.setVisibility(View.GONE);
+        btn_next.setText("Keren !!!");
+        isFragmentDisplayed = true;
+    }
+
+    public void closeFragment(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        SecondFragment secondFragment = (SecondFragment) fragmentManager.findFragmentById(R.id.fragment1);
+        if(secondFragment != null){
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.remove(secondFragment).commit();
+        }
+        fragment.setVisibility(View.VISIBLE);
+        btn_next.setText("Lihat Sejarah Bali");
+        isFragmentDisplayed = false;
     }
 }
---
+++ app/src/main/java/com/example/fragmentnewww/SecondFragment.java
@@ -0,0 +1,28 @@
+package com.example.fragmentnewww;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SecondFragment extends Fragment {
+
+    public SecondFragment() {
+        // Required empty public constructor
+    }
+
+    public static SecondFragment newInstance() {
+        return new SecondFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View view = inflater.inflate(R.layout.fragment_second, container, false);
+        return view;
+    }
+}
